4a103722ae17636d376f8b40e88cfb1e62cbd25d
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-06-02 03:45:21 +0200

    Log in with Google on main activity start

--- app/src/main/java/com/brittlepins/brittleup/MainActivity.java
+++ app/src/main/java/com/brittlepins/brittleup/MainActivity.java
@@ -2,13 +2,35 @@ package com.brittlepins.brittleup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public class MainActivity extends AppCompatActivity {
+    GoogleSignInAccount mAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAccount = getIntent().getParcelableExtra("Google account");
+
+        if (mAccount == null) {
+            Intent intent = new Intent(this, SignInActivity.class);
+            startActivity(intent);
+        } else {
+            if (mAccount != null) {
+                Log.i("Get account from intent", mAccount.getEmail());
+            } else {
+                Log.i("Get account from intent", "no account");
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/brittlepins/brittleup/SignInActivity.java
@@ -0,0 +1,66 @@
+package com.brittlepins.brittleup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class SignInActivity extends AppCompatActivity {
+    private final int RC_SIGN_IN = 1;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    public void signInWithGoogle(View view) {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private
+
+    void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            updateUI(account);
+        } catch (ApiException ex) {
+            Log.e("Handle sign in result", "Failed to sign in:" + ex.getMessage());
+            ex.printStackTrace();
+            updateUI(null);
+        }
+    }
+
+    void updateUI(GoogleSignInAccount account) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("Google account", account);
+        startActivity(intent);
+    }
+}
