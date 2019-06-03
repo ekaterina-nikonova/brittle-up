d58c807b7b3b676e00f4578f229079d1be714926
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-06-03 09:07:24 +0200

    Upload text file on button click

--- app/src/main/java/com/brittlepins/brittleup/DriveService.java
+++ app/src/main/java/com/brittlepins/brittleup/DriveService.java
@@ -1,7 +1,47 @@
 package com.brittlepins.brittleup;
 
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.api.client.http.ByteArrayContent;
 import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class DriveService {
-    public DriveService(Drive drive) {}
+    private final Executor mExecutor = Executors.newSingleThreadExecutor();
+    private final Drive mDrive;
+
+    public DriveService(Drive drive) {
+        mDrive = drive;
+    }
+
+    public Task<String> createFile() {
+        return Tasks.call(mExecutor, () -> {
+            File metadata = new File()
+                    .setParents(Collections.singletonList("root"))
+                    .setMimeType("image/jpeg")
+                    .setName("test-image");
+
+            File imageFile = mDrive.files().create(metadata).execute();
+
+            if (imageFile == null) {
+                throw new IOException("Null result when requesting image file creation.");
+            }
+
+            return imageFile.getId();
+        });
+    }
+
+    public Task<Void> saveFile(String fileId, String name, String content) {
+        return Tasks.call(mExecutor, () -> {
+            File metadata = new File().setName(name);
+            ByteArrayContent contentStream = ByteArrayContent.fromString("text/plain", content);
+            mDrive.files().update(fileId, metadata, contentStream).execute();
+            return null;
+        });
+    }
 }
